f459f65cba5726939653fbcad79911bc3198deee
beepwms <44948609+beepwms@users.noreply.github.com>
2018-11-11 18:02:26 +0100

    Create users.sql

---
+++ postgree/tables/beepcore/users.sql
@@ -0,0 +1,29 @@
+-- Table: beepcore.users
+
+-- DROP TABLE beepcore.users;
+
+CREATE TABLE beepcore.users
+(
+    usr_id integer NOT NULL,
+    usr_dtins timestamp with time zone,
+    usr_userins character varying(50) COLLATE pg_catalog."default",
+    usr_trnins bigint,
+    usr_dtupd timestamp without time zone,
+    usr_userupd character varying(50) COLLATE pg_catalog."default",
+    usr_trnupd bigint,
+    usr_username character varying(50) COLLATE pg_catalog."default",
+    usr_password character varying(50) COLLATE pg_catalog."default",
+    usr_name character varying(50) COLLATE pg_catalog."default",
+    usr_surname character varying(50) COLLATE pg_catalog."default",
+    usr_email character varying(50) COLLATE pg_catalog."default",
+    usr_phone character varying(30) COLLATE pg_catalog."default",
+    usr_company character varying(30) COLLATE pg_catalog."default",
+    CONSTRAINT users_pkey PRIMARY KEY (usr_id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE beepcore.users
+    OWNER to postgres;
